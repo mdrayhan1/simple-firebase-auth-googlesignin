02c54788ce0d43fce92b48388b811098ead232a8
Md. Rayhan <mdrayhanm252@gmail.com>
2022-10-19 09:00:22 +0600

    google sign in out button toggle mens jokhon sign in thakbe tokho signout button dekhabe arekta biporit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import "./App.css";
 import app from "./firebase/firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
   const provider = new GoogleAuthProvider();
-  
 
-
-
-  
-
-  const [user, setUser]= useState({})
-
-
-
-
+  const [user, setUser] = useState({});
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
 
-
-
         setUser(user);
-
-
 
         console.log(user);
       })
@@ -37,20 +24,28 @@ function App() {
       });
   };
 
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then(() =>{
+      setUser({});
+    })
+    .error(() =>{
+      setUser({});
+
+    })
+  }
   return (
     <div className="app">
-      <button onClick={handleGoogleSignIn}>Goole Sign In</button>
-      <div>
-
-
-
+      {/* condition ? true : false */}
+      {user.email ?
+        <button onClick={handleSignOut}>Sign out</button>
+        :
+        <button onClick={handleGoogleSignIn}>Goole Sign In</button>
+      }
+      {user.email && <div>
         <h3>User name: {user.displayName}</h3>
         <p>Email address: {user.email}</p>
-
-
-
-
-      </div>
+      </div>}
     </div>
   );
 }
