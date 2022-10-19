7c3f62fcf9f9952951dcda1f999530946144a856
Md. Rayhan <mdrayhanm252@gmail.com>
2022-10-19 06:56:58 +0600

    strps file included

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import app from './firebase/firebase.init';
+import "./App.css";
+import app from "./firebase/firebase.init";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
-
 function App() {
   const provider = new GoogleAuthProvider();
 
-  const handleGoogleSignIn = () =>{
+  const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-    .then(result =>{
-      const user = result.user;
-      console.log(user);
-
-    })
-    .catch(error  =>{
-      console.log('error ', error);
-    })
-  }
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("error ", error);
+      });
+  };
 
   return (
     <div className="">
